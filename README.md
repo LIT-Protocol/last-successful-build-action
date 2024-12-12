1061de7e053945906ca4704a756b97040c8fbf0b
Chris Cassano <chris@litprotocol.com>
2024-12-11 18:16:50 -0800

    add repo field

--- README.md
+++ README.md
@@ -1,49 +1,54 @@
 # last-successful-build-action
 
-This action searches for the last successful workflow-run for the given workflow-name and branch. 
+This action searches for the last successful workflow-run for the given workflow-name and branch.
 The `sha` of the workflow-commit is set as output-parameter. If no matching workflow exists, the `sha` of the current run is emitted.
+
+## Lit-Protocol fork
+
+The only change in this fork is the ability to do this for another repo, instead of the current one the action is running in.
 
 ## Usage
 
 ```yml
-      - uses: actions/checkout@v3
-      - name: Find matching workflow
-        uses: SamhammerAG/last-successful-build-action@v4
-        with:
-          token: "${{ secrets.GITHUB_TOKEN }}"
-          branch: "development"
-          workflow: "build"
+- uses: actions/checkout@v3
+- name: Find matching workflow
+  uses: SamhammerAG/last-successful-build-action@v4
+  with:
+    token: "${{ secrets.GITHUB_TOKEN }}"
+    branch: "development"
+    workflow: "build"
 ```
 
 ## Verifying workflow run SHAs
+
 If your workflow runs are expected to contain no-longer existing commit SHAs (e.g. when squashing and force pushing) you need to verify the SHA of the workflow run commit against the list of commit SHAs in your repository.
 
 ```yml
-      - uses: actions/checkout@v3
-        with:
-          fetch-depth: 0 # check out the entire repo history for SHA verification
-      - name: Find matching workflow
-        uses: SamhammerAG/last-successful-build-action@v4
-        with:
-          branch: "development"
-          workflow: "build"
-          verify: true
+- uses: actions/checkout@v3
+  with:
+    fetch-depth: 0 # check out the entire repo history for SHA verification
+- name: Find matching workflow
+  uses: SamhammerAG/last-successful-build-action@v4
+  with:
+    branch: "development"
+    workflow: "build"
+    verify: true
 ```
 
 ## Config
+
 ### Action inputs
 
-| Name | Description | Default |
-| --- | --- | --- |
-| `token` | `GITHUB_TOKEN` or a `repo` scoped [PAT](https://docs.github.com/en/github/authenticating-to-github/creating-a-personal-access-token). | `GITHUB_TOKEN` |
-| `branch` | Branch for the workflow to look for. | "" |
-| `workflow` | Workflow name to look for. | "" |
-| `job` | Job name to look for. | "" |
-| `verify` | Verify workflow commit SHA against list of SHAs in repository | `false` |
-
+| Name       | Description                                                                                                                           | Default        |
+| ---------- | ------------------------------------------------------------------------------------------------------------------------------------- | -------------- |
+| `token`    | `GITHUB_TOKEN` or a `repo` scoped [PAT](https://docs.github.com/en/github/authenticating-to-github/creating-a-personal-access-token). | `GITHUB_TOKEN` |
+| `branch`   | Branch for the workflow to look for.                                                                                                  | ""             |
+| `workflow` | Workflow name to look for.                                                                                                            | ""             |
+| `job`      | Job name to look for.                                                                                                                 | ""             |
+| `verify`   | Verify workflow commit SHA against list of SHAs in repository                                                                         | `false`        |
 
 ### Action outputs
 
-| Name | Description | Default |
-| --- | --- | --- |
+| Name  | Description                                        | Default             |
+| ----- | -------------------------------------------------- | ------------------- |
 | `sha` | Sha of the workflow-run matching the requirements. | `${{ github.sha }}` |
--- src/main.ts
+++ src/main.ts
@@ -1,124 +1,147 @@
-import * as core from '@actions/core'
+import * as core from "@actions/core";
 import * as github from "@actions/github";
-import { exec } from 'child_process';
-import { promisify } from 'util';
+import { exec } from "child_process";
+import { promisify } from "util";
 
 const execAsync = promisify(exec);
 
 let repoShas: string[] | undefined;
 
-const verifyCommit =  async (sha: string): Promise<boolean> => {
-    if (!repoShas) {
-        try {
-            const cmd = `git log --format=format:%H`;
-            core.info(`Getting list of SHAs in repo via command "${cmd}"`);
+const verifyCommit = async (sha: string): Promise<boolean> => {
+  if (!repoShas) {
+    try {
+      const cmd = `git log --format=format:%H`;
+      core.info(`Getting list of SHAs in repo via command "${cmd}"`);
 
-            const { stdout } = await execAsync(cmd);
+      const { stdout } = await execAsync(cmd);
 
-            repoShas = stdout.trim().split('\n');
-        } catch (e: any) {
-            repoShas = [];
-            core.warning(`Error while attempting to get list of SHAs: ${e?.message}`);
+      repoShas = stdout.trim().split("\n");
+    } catch (e: any) {
+      repoShas = [];
+      core.warning(`Error while attempting to get list of SHAs: ${e?.message}`);
 
-            return false;
-        }
+      return false;
     }
+  }
 
-    core.info(`Looking for SHA ${sha} in repo SHAs`);
+  core.info(`Looking for SHA ${sha} in repo SHAs`);
 
-    return repoShas.includes(sha);
-}
+  return repoShas.includes(sha);
+};
 
 async function run(): Promise<void> {
-    try {
-        const inputs = {
-            token: core.getInput("token"),
-            branch: core.getInput("branch"),
-            workflow: core.getInput("workflow"),
-            job: core.getInput("job"),
-            verify: core.getInput("verify") === "true" ? true : false
-        };
+  try {
+    const inputs = {
+      token: core.getInput("token"),
+      branch: core.getInput("branch"),
+      workflow: core.getInput("workflow"),
+      job: core.getInput("job"),
+      verify: core.getInput("verify") === "true" ? true : false,
+      repo: core.getInput("repo"),
+    };
 
-        const octokit = github.getOctokit(inputs.token);
-        const repository: string = process.env.GITHUB_REPOSITORY as string;
-        const [owner, repo] = repository.split("/");
+    const octokit = github.getOctokit(inputs.token);
+    const repository: string = inputs.repo;
+    const [owner, repo] = repository.split("/");
 
-        const workflows = await octokit.rest.actions.listRepoWorkflows({ owner, repo });
-        const workflowId = workflows.data.workflows.find(w => w.name.toLowerCase() === inputs.workflow.toLowerCase())?.id;
+    const workflows = await octokit.rest.actions.listRepoWorkflows({
+      owner,
+      repo,
+    });
+    const workflowId = workflows.data.workflows.find(
+      (w: any) => w.name.toLowerCase() === inputs.workflow.toLowerCase()
+    )?.id;
 
-        if (!workflowId) {
-            core.setFailed(`No workflow exists with the name "${inputs.workflow}"`);
-            return;
-        } else {
-            core.info(`Discovered workflowId for search: ${workflowId}`);
-        }
-
-        const response = await octokit.rest.actions.listWorkflowRuns({ owner, repo, workflow_id: workflowId, per_page: 100 });
-        const runs = response.data.workflow_runs
-            .filter(x => (!inputs.branch || x.head_branch === inputs.branch) && (inputs.job || x.conclusion === "success"))
-            .sort((r1, r2) => new Date(r2.created_at).getTime() - new Date(r1.created_at).getTime());
-
-        let triggeringSha = process.env.GITHUB_SHA as string;
-        let sha: string | undefined = undefined;
-        let lastSha: string | undefined;
-        
-        if (runs.length > 0) {
-            for (const run of runs) {
-                lastSha = run.head_sha;
-                core.debug(`This SHA: ${triggeringSha}`);
-                core.debug(`Run SHA: ${run.head_sha}`);
-                core.debug(`Run Branch: ${run.head_branch}`);
-                core.debug(`Wanted branch: ${inputs.branch}`);
-
-                if (inputs.branch && run.head_branch !== inputs.branch){
-                    continue;
-                }
-
-                if (inputs.verify && !await verifyCommit(run.head_sha)) {
-                    core.warning(`Failed to verify commit ${run.head_sha}. Skipping.`);
-                    continue;
-                }
-
-
-                if(inputs.job) {
-                    const jobs = await octokit.rest.actions.listJobsForWorkflowRun({ owner, repo, run_id: run.id });
-
-                    let foundJob = false;
-                    for(const job of jobs.data.jobs) {
-                        if(job.name === inputs.job) {
-                            if(job.conclusion === "success") {
-                                foundJob = true;
-                                break;
-                            }
-                        }
-                    }
-                    if(!foundJob) {
-                        continue;
-                    }
-                }
-
-                core.info(
-                    inputs.verify
-                    ? `Commit ${run.head_sha} from run ${run.html_url} verified as last successful CI run.`
-                    : `Using ${run.head_sha} from run ${run.html_url} as last successful CI run.`
-                );
-                sha = run.head_sha;
-
-                break;
-            }
-        } else {
-            core.info(`No previous runs found for branch ${inputs.branch}.`);
-        }
-
-        if (!sha) {
-            core.warning(`Unable to determine SHA of last successful commit (possibly outside the window of ${runs.length} runs). Using earliest SHA available.`);
-            sha = lastSha;
-        }
-
-        core.setOutput('sha', sha);
-    } catch (error: any) {
-        core.setFailed(error?.message);
+    if (!workflowId) {
+      core.setFailed(`No workflow exists with the name "${inputs.workflow}"`);
+      return;
+    } else {
+      core.info(`Discovered workflowId for search: ${workflowId}`);
     }
+
+    const response = await octokit.rest.actions.listWorkflowRuns({
+      owner,
+      repo,
+      workflow_id: workflowId,
+      per_page: 100,
+    });
+    const runs = response.data.workflow_runs
+      .filter(
+        (x) =>
+          (!inputs.branch || x.head_branch === inputs.branch) &&
+          (inputs.job || x.conclusion === "success")
+      )
+      .sort(
+        (r1, r2) =>
+          new Date(r2.created_at).getTime() - new Date(r1.created_at).getTime()
+      );
+
+    let triggeringSha = process.env.GITHUB_SHA as string;
+    let sha: string | undefined = undefined;
+    let lastSha: string | undefined;
+
+    if (runs.length > 0) {
+      for (const run of runs) {
+        lastSha = run.head_sha;
+        core.debug(`This SHA: ${triggeringSha}`);
+        core.debug(`Run SHA: ${run.head_sha}`);
+        core.debug(`Run Branch: ${run.head_branch}`);
+        core.debug(`Wanted branch: ${inputs.branch}`);
+
+        if (inputs.branch && run.head_branch !== inputs.branch) {
+          continue;
+        }
+
+        if (inputs.verify && !(await verifyCommit(run.head_sha))) {
+          core.warning(`Failed to verify commit ${run.head_sha}. Skipping.`);
+          continue;
+        }
+
+        if (inputs.job) {
+          const jobs = await octokit.rest.actions.listJobsForWorkflowRun({
+            owner,
+            repo,
+            run_id: run.id,
+          });
+
+          let foundJob = false;
+          for (const job of jobs.data.jobs) {
+            if (job.name === inputs.job) {
+              if (job.conclusion === "success") {
+                foundJob = true;
+                break;
+              }
+            }
+          }
+          if (!foundJob) {
+            continue;
+          }
+        }
+
+        core.info(
+          inputs.verify
+            ? `Commit ${run.head_sha} from run ${run.html_url} verified as last successful CI run.`
+            : `Using ${run.head_sha} from run ${run.html_url} as last successful CI run.`
+        );
+        sha = run.head_sha;
+
+        break;
+      }
+    } else {
+      core.info(`No previous runs found for branch ${inputs.branch}.`);
+    }
+
+    if (!sha) {
+      core.warning(
+        `Unable to determine SHA of last successful commit (possibly outside the window of ${runs.length} runs). Using earliest SHA available.`
+      );
+      sha = lastSha;
+    }
+
+    core.setOutput("sha", sha);
+  } catch (error: any) {
+    core.setFailed(error?.message);
+  }
 }
 
 run();
